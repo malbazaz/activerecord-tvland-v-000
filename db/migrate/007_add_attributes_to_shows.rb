97cff51622d4c126e7068499a9b154bb48eb7f21
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 01:43:49 +0000

    automatically backed up by learn

--- db/migrate/007_add_attributes_to_shows.rb
+++ db/migrate/007_add_attributes_to_shows.rb
@@ -3,6 +3,7 @@ class AddAttributesToShows < ActiveRecord::Migration[4.2]
   #a new migration, and then in the change method,
   #we make the change.
   def change
-    add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
