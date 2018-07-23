578c0e0236fc7d544134a60efb0561cb255065f6
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 01:15:49 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,3 +1,11 @@
 class CreateActors < ActiveRecord::Migration
 
-  
+  def change
+    create_table actors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.array :characters
+    end
+  end
+
+end
