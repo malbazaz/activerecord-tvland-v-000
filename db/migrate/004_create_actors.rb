42b5ef7ed110c79f8b4db54a38d0a718922f0ac3
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 01:19:32 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,7 @@
 class CreateActors < ActiveRecord::Migration
 
   def change
-    create_table actors do |t|
+    create_table :actors do |t|
       t.string :first_name
       t.string :last_name
     end
