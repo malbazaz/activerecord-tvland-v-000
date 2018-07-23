27228206f4aa8d7d04966b2c57cf62f9af291c50
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 01:51:47 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network 
 
   def build_network(whatever)
     network_found = Network.find_by(whatever)
