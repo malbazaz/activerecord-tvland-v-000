9cf465eb6064797f066a522b7fff976f8f1afb3e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 01:27:30 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,7 +4,7 @@ class CreateCharacters < ActiveRecord::Migration
     create_table :characters do |t|
       t.string :name
       t.integer :actor_id
-      t.integer :show_id 
+      t.integer :show_id
     end
   end
 
