c37ddd19ef8f736905ec2dcac8ec1aab3373b771
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 02:02:44 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,8 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
 
-attr_accessor :catchphrase
-
+def catchphrase 
+  "#{self.name} always says: #{self.catchphrase}"
+end 
 
 end
